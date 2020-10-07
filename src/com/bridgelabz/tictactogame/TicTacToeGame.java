c473ce4995a5d417fc8399f359bd17750b82ba16
Kauk777 <curokky777@gmail.com>
2020-10-07 10:39:39 +0530

    [KAUSHAL]Added: UC3 user cell

--- src/com/bridgelabz/tictactogame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactogame/TicTacToeGame.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	public static void main(String[] args) {
+		Scanner userInput=new Scanner(System.in);
 		char[] gameBoard=createGameBoard();
-		char playerSymbol=choosePlayerSymbol(); 
-		char computerSymbol=chooseComputerSymbol();
+		char userLetter = chooseUserLetter(userInput);
+		char computerLetter=(userLetter=='X')? 'O' : 'X';
+		int userCell=showBoard(userInput);
+		
 	}
+	//Creating Game Board
 	public static char[] createGameBoard() {
 		char[] gameBoard=new char[10];
 		for(int i=1;i<gameBoard.length;i++)
 			gameBoard[i]=' ';
 		return gameBoard;
 	}
-	public static char choosePlayerSymbol() {
-		Scanner sc=new Scanner(System.in);
-		char choose=sc.next().charAt(0);
-		return choose;
+	//choosing player symbol
+	public static char chooseUserLetter(Scanner userInput) {
+		System.out.println("Choose player symbol 'X' or 'O'");
+		return userInput.next().toUpperCase().charAt(0);
 	}
-	public static char chooseComputerSymbol() {
-		char playerSymbol=choosePlayerSymbol();
-		if(playerSymbol=='X')
-			return 'O';
-		else
-			return 'X';	
+	// UC3 choosing User Cell
+	public static int showBoard(Scanner userInput) {
+		System.out.println("choose valid cell 1-9");
+		return userInput.nextInt();
 	}
 }
